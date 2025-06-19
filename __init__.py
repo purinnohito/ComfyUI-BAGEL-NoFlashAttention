f7a958b60f29499c5ff4fadcfd2be3f86f4f0ac7
Fayche <1751162157@qq.com>
2025-06-20 02:12:33 +0800

    feat: Rename nodes.py and bump into 1.0.6

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 ComfyUI-Bagel - ComfyUI custom node package for the BAGEL multimodal model
 """
 
-from .node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 # Export node mappings for ComfyUI
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
